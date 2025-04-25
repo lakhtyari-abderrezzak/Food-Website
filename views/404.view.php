d9ede5b92bd4891339798874c341ec5a9df02031
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-25 12:46:22 +0100

    feat: add 404.view.php to views folder

---
+++ views/404.view.php
@@ -0,0 +1,9 @@
+<?php require_once 'views/partials/header.view.php'; ?>
+<div class="error-page">
+    <div class="container">
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+        <a href="/" class="btn btn-primary">Go to Home</a>
+</div>
+</div>
+<?php require_once 'views/partials/footer.view.php'; ?>
