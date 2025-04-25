b81e8ccf24c32b07855aaebecdcf59f398c53335
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-25 12:37:07 +0100

    feat: initialize Router in index.php and load routes from routes.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once 'Core/Router.php';
+
+$router = new Router();
+
+// Load all routes
+require 'routes.php';
+
+// Get the requested URI (excluding query strings like ?id=1)
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Route the request
+$router->route($uri);
