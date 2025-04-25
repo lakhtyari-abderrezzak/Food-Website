d09313c47a824454bcbb42eb26794712231d412c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-25 12:40:37 +0100

    feat: add footer.view.php with basic HTML footer layout

---
+++ views/partials/footer.view.php
@@ -0,0 +1,30 @@
+<!-- social Section Starts Here -->
+
+<!-- social Section Ends Here -->
+
+<!-- footer Section Starts Here -->
+<div class="footer text-center">
+    <div class="social">
+        <div class="container ">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
+
+</div>
+<!-- footer Section Ends Here -->
+
+</body>
+
+</html>
