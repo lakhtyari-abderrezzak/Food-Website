a9f414c683aabe6d45e7e2ee41ddcba4016433a9
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:18:13 +0100

    feat: update routes for food and contact pages, and organize admin routes

--- routes.php
+++ routes.php
@@ -3,6 +3,16 @@
 
 $router->add('/', 'controllers/guest.php');
 $router->add('/categories', 'controllers/categories/index.php');
-$router->add('/categories/{$id}', 'controllers/admin/show.php');
-$router->add('/categories/{$id}', 'controllers/admin/edit.php');
-$router->add('/dashboard/create', 'controllers/admin/create.php');
+$router->add('/food', 'controllers/food/index.php');
+$router->add('/contact', 'controllers/contact.php');
+
+// Admin routes
+
+$router->add('/dashboard', 'admin/controllers/dashboard.php');
+
+
+// login and register routes
+$router->add('/login', 'controllers/auth/login.php');
+$router->add('/logout', 'controllers/auth/logout.php');
+$router->add('/register', 'controllers/auth/register.php');
+
