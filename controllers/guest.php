f4945fd7b5fa0f30fc7906de7b2524bd8982ea76
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:22:35 +0100

    feat: implement food fetching logic in guest controller

--- controllers/guest.php
+++ controllers/guest.php
@@ -1,3 +1,16 @@
 <?php
 
+require 'config.php';
+
+// Fetching featured foods from the database
+$stmt = $conn->prepare("SELECT * FROM food WHERE featured = true ORDER BY id DESC  LIMIT 3");
+$stmt->execute();
+$featured_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Fetching all foods from the database
+$stmt = $conn->prepare("SELECT * FROM food ORDER BY id DESC LIMIT 6");  
+$stmt->execute();
+$all_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 require './views/guest.view.php';
