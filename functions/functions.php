86a266925f7171a4cc2d47ec61e1def7aaec750c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:24:02 +0100

    feat: add functions to fetch all records and find a record by ID from the database

--- functions/functions.php
+++ functions/functions.php
@@ -11,3 +11,19 @@ function redirect($path)
     header("Location: $path");
     exit;
 }
+
+function all($table, $conn)
+{
+    $sql = "SELECT * FROM $table";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+function find($table, $id, $conn)
+{
+    $sql = "SELECT * FROM $table WHERE id = :id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
