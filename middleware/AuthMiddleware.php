3054824abeb1ca5bd4887c477deedc5e0995fe45
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:26:27 +0100

    feat: add authentication middleware to handle user session management

---
+++ middleware/AuthMiddleware.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+namespace Middleware;
+
+class AuthMiddleware
+{
+    public function handle()
+    {
+        session_start();
+
+        if (!isset($_SESSION['user_id'])) {
+            header("Location: /login");
+            exit();
+        }
+    }
+}
