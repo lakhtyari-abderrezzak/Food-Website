268e83c2cbd1f6c69132d966af8aa6b9b56cd1b6
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:23:39 +0100

    feat: add category fetching logic from the database

--- controllers/categories/index.php
+++ controllers/categories/index.php
@@ -1,3 +1,14 @@
 <?php 
+require 'config.php';
+
+
+
+// Fetch categories from the database
+$sql = "SELECT * FROM categories";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 require './views/categories/index.view.php';
