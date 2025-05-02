cbc9460430861686ed68590d77c99aeb1533b631
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-02 01:17:42 +0100

    feat: add database connection configuration in config.php

--- config.php
+++ config.php
@@ -1,1 +1,19 @@
 <?php 
+
+// Database configuration
+$host = 'localhost';
+$db = 'food_db';
+$username = 'root';
+$password = ''; 
+
+// Create a connection to the database
+
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
