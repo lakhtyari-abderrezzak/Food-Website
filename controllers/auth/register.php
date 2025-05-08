4feb7eee0c1ba69929102ba76af158fbe372df33
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-08 01:31:26 +0100

    feat: add account activation email functionality after user registration

--- controllers/auth/register.php
+++ controllers/auth/register.php
@@ -18,7 +18,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $password_confirmation = $_POST['password_confirmation'];
 
-    // ✅ Validation
+
+
+    //  Validation
     if (empty($username) || strlen($username) < 3) {
         $errors['name'] = 'Name should be at least 3 characters long';
     } elseif (strlen($username) > 255) {
@@ -52,20 +54,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         require_once 'config.php'; // Ensure $pdo exists
         $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
+        $activation_token = bin2hex(random_bytes(16)); // Generate a random token
+        $activation_token_hash = hash('sha256', $activation_token); // Hash the token for storage
+        
+        $stmt = $conn->prepare('INSERT INTO `admin` (username, email, `password`, account_activation_hash) VALUES (?, ?, ?, ?)');
+        $stmt->execute([$username, $email, $hashed_password, $activation_token_hash]);
+        if ($stmt->rowCount() > 0) {
+            require_once  './controllers/mail.php';
+        
+            $activation_link = 'http://localhost:8888/activation?token=' . $activation_token;
+            $mail->setFrom('noreply@gmail.com', 'Food App');
+            $mail->addAddress($_POST['email']);
+            $mail->Subject = 'Activate Your Account';
+        
+            $mail->Body =  <<<END
+        
+                <p>Hi,</p>
+                <p>Thank you for registering. Click the link below to activate your account:</p>
+                <p><a href="$activation_link">Click here</a></p>
+                <p>If you did not request this, please ignore this email.</p>
+                <p>Thank you!</p>
+            END;
 
-
-        $stmt = $conn->prepare('INSERT INTO `admin` (username, email, password) VALUES (?, ?, ?)');
-        $stmt->execute([$username, $email, $hashed_password]);
-
-        $_SESSION['user'] = [
-            'name' => $username,
-            'email' => $email
-        ];
-
-        header('Location: /');
-        exit;
+            require_once './views/auth/register-success.view.php';
+        
+            try{
+                $mail->send();
+            } catch (Exception $e) {
+                echo "Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
+        
     } else {
-        $_SESSION['errors'] = $errors;
+        $_SESSION['register_errors'] = $errors;
         $_SESSION['old'] = [
             'name' => $username,
             'email' => $email
