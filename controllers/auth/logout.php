53c3138d4f86f5e041219aa48ef1f7a440251c2d
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-08 01:30:31 +0100

    feat: enhance logout functionality to clear remember_me fields and session variables

--- controllers/auth/logout.php
+++ controllers/auth/logout.php
@@ -1,13 +1,30 @@
 <?php
-// Start the session
 session_start();
 
-// Unset all session variables
-$_SESSION = array();
+require_once './config.php';
+
+$sql = "SELECT * FROM `admin` WHERE email = ? LIMIT 1";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$_SESSION['user']['email']]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($user) {
+    // Clear the remember_me fields in the database
+    $stmt = $conn->prepare("UPDATE `admin` SET remember_selector = NULL, remember_token = NULL, remember_expires = NULL WHERE email = ?");
+    $stmt->execute([$_SESSION['user']['email']]);
+}
+
+// Remove all session variables
+$_SESSION = [];
 
 // Destroy the session
 session_destroy();
 
-// Redirect to the login page or home page
-header("Location: /login");
+// Clear the "remember_me" cookie (if set)
+if (isset($_COOKIE['remember_me'])) {
+    setcookie('remember_me', '', time() - 3600, "/", "", true, true);
+}
+
+
+
+header('Location: /login');
 exit;
