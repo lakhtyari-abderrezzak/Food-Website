0cb677117fd8511bbf1f0ed0e880d104d83b39da
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-11 00:52:33 +0100

    feat: add success message display component to show session success notifications

---
+++ views/components/success.view.php
@@ -0,0 +1,6 @@
+<?php if (isset($_SESSION['success'])): ?>
+        <div class="max-w-xl mx-auto mt-4 bg-green-100 text-green-800 px-4 py-3 rounded shadow">
+            <?= $_SESSION['success'] ?>
+            <?php unset($_SESSION['success']); ?>
+        </div>
+    <?php endif; ?>
