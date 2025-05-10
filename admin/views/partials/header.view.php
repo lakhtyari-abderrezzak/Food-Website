8332bedc48e2b94c5ca64ac46f2966977b204c53
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-11 00:51:59 +0100

    fix: update navigation links to use the correct dashboard paths

--- admin/views/partials/header.view.php
+++ admin/views/partials/header.view.php
@@ -11,12 +11,12 @@
     <div class="container mx-auto flex justify-between">
         <div class="text-white font-bold text-xl">Food Website Admin</div>
         <div class="space-x-4">
-            <a href="/admin" class="text-white hover:underline">Dashboard</a>
-            <a href="/admin/foods" class="text-white hover:underline">Foods</a>
-            <a href="/admin/categories" class="text-white hover:underline">Categories</a>
-            <a href="/admin/orders" class="text-white hover:underline">Orders</a>
+            <a href="/dashboard" class="text-white hover:underline">Dashboard</a>
+            <a href="/dashboard/foods" class="text-white hover:underline">Foods</a>
+            <a href="/dashboard/categories" class="text-white hover:underline">Categories</a>
+            <a href="/dashboard/orders" class="text-white hover:underline">Orders</a>
             <a href="/logout" class="text-white hover:underline">Logout</a>
         </div>
     </div>
 </nav>
-<div class="container mx-auto">
+<div class="container mx-auto h-screen">
