87510f7314497f78db5581619c6bbfbeb55fae68
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-21 00:41:26 +0100

    feat: implement search functionality for food items in welcome controller

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -2,13 +2,16 @@
 
 require 'config.php';
 
+$search = $_GET['search'] ?? null;
+
 // Fetching featured foods from the database
 $stmt = $conn->prepare("SELECT * FROM food WHERE featured = true ORDER BY id DESC  LIMIT 3");
 $stmt->execute();
 $featured_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Fetching all foods from the database
-$stmt = $conn->prepare("SELECT * FROM food ORDER BY id DESC LIMIT 6");  
+$stmt = $conn->prepare("SELECT * FROM food WHERE food.title LIKE :search ORDER BY id DESC LIMIT 20");
+$stmt->bindValue(':search', "%$search%");     
 $stmt->execute();
 $all_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
