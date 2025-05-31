2a1000d8e5720ea79fff6b4f731a222b0f72e419
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-05-31 15:08:24 +0100

    fix: move clearCart function definition above cart items initialization

--- controllers/cart/index.php
+++ controllers/cart/index.php
@@ -5,4 +5,8 @@ require_once './bootstrap.php';
 $cartItems = $_SESSION['cart'] ?? [];
 $total = 0;
 
+function clearCart() {
+    unset($_SESSION['cart']);
+}
+
 require_once './views/cart/index.view.php';
