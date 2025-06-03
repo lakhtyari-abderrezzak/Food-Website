aaaa425785546e1516df4a5166d59ff996a716a3
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-06-04 00:41:11 +0100

    fix: remove search filter from food retrieval query in welcome controller

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -10,8 +10,8 @@ $stmt->execute();
 $featured_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Fetching all foods from the database
-$stmt = $conn->prepare("SELECT * FROM food WHERE food.title LIKE :search ORDER BY id DESC LIMIT 20");
-$stmt->bindValue(':search', "%$search%");     
+$stmt = $conn->prepare("SELECT * FROM food  ORDER BY id DESC LIMIT 20");
+    
 $stmt->execute();
 $all_foods = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
